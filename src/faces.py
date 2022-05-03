1cabc5413fd04fbc9ba1e77c54e2e392170f26a0
occasionalcode <occasionalprogrammer@gmail.com>
2022-05-03 20:32:02 +0800

    with data faces

--- src/faces.py
+++ src/faces.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 
 # test one cascade
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -10,24 +11,26 @@ while(True):
     # Capture frame by frame
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
-    for (x,y,w,h) in faces:
-        print(x,y,w,h)
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+
         img_item = "my-image.png"
         cv2.imwrite(img_item, roi_gray)
 
-        color = (255,0,0) 
+        color = (255, 0, 0)
         stroke = 2
         width = x + w
         height = y + h
-        cv2.rectangle(frame, (x,y), (width,height), color, stroke)
- 
-    #Display the resulting frame 
-    cv2.imshow('frame',frame)
+        cv2.rectangle(frame, (x, y), (width, height), color, stroke)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-cap.release()   
-cv2.destroyAllWindows() 
+cap.release()
+cv2.destroyAllWindows()

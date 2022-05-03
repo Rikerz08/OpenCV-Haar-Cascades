1a2462717400b160d18e650e0a06dcb1f53d2968
Riki <rikikarl8@gmail.com>
2022-05-03 17:01:10 +0800

    cascades copied, rectangle set

---
+++ src/faces.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+# test one cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame by frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (255,0,0) 
+        stroke = 2
+        width = x + w
+        height = y + h
+        cv2.rectangle(frame, (x,y), (width,height), color, stroke)
+ 
+    #Display the resulting frame 
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()   
+cv2.destroyAllWindows() 
